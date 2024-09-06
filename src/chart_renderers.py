84b3986a1c9171bdfaa08694ec921660896f4852
mashriram <mashriram@gmail.com>
2024-09-06 07:33:28 +0530

    feat: added stacked line and bars

---
+++ src/chart_renderers.py
@@ -0,0 +1,183 @@
+from streamlit_echarts import st_echarts
+import pandas as pd
+from streamlit_echarts import JsCode
+
+
+def create_stacked_line_chart(df, by, column):
+
+    by_cols = ["year"]
+    by_cols.append(by)
+    avg_price_year_commodity = round(df.groupby(by_cols)[column].agg("mean"))
+    price_comm = avg_price_year_commodity.reset_index()
+    print(price_comm, avg_price_year_commodity)
+    years: list[str] = avg_price_year_commodity.index.levels[0].tolist()
+    commodities: list[str] = avg_price_year_commodity.index.levels[1].tolist()
+    print(commodities, years)
+
+    series = [
+        {
+            "name": commodity,
+            "type": "line",
+            "stack": "Total",
+            "data": price_comm.query(f"{by} == '{commodity}'")[f"{column}"].tolist(),
+        }
+        for year, commodity in zip(years, commodities)
+    ]
+
+    options = {
+        "title": {"text": "Commodity Price over time"},
+        "tooltip": {"trigger": "axis"},
+        "legend": {"data": commodities},
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "toolbox": {"feature": {"saveAsImage": {}}},
+        "xAxis": {
+            "type": "category",
+            "boundaryGap": False,
+            "data": years,
+        },
+        "yAxis": {"type": "value"},
+        "series": series,
+    }
+
+    st_echarts(options=options, height="400px")
+
+
+def create_line_chart(df, column):
+    years = sorted(list(df["year"].astype(str).unique()))
+    print(years)
+
+    avg_price_year = round(df.groupby(["year"])[column].agg("mean")).tolist()
+
+    options = {
+        "xAxis": {
+            "type": "category",
+            "data": years,
+        },
+        "yAxis": {"type": "value"},
+        "series": [{"data": avg_price_year, "type": "line"}],
+    }
+
+    st_echarts(options=options)
+
+
+def create_stacked_bar_chart(df, by: str, column):
+    years = sorted(list(df["year"].astype(str).unique()))
+    by_cols = ["year"]
+    by_cols.append(by)
+    avg_price_year_commodity = round(df.groupby(by_cols)[column].agg("mean"))
+
+    commodities = avg_price_year_commodity.index.levels[1].tolist()
+    print("By Cols", (commodities))
+
+    price_comm = avg_price_year_commodity.reset_index()
+
+    series = [
+        {
+            "name": commodity,
+            "type": "bar",
+            "stack": "Total",
+            "label": {"show": True},
+            "emphasis": {"focus": "series"},
+            "data": price_comm.query(f"{by} == '{commodity}'")[f"{column}"].tolist(),
+        }
+        for year, commodity in zip(years, commodities)
+    ]
+
+    options = {
+        "tooltip": {"trigger": "axis", "axisPointer": {"type": "shadow"}},
+        "legend": {"data": commodities},
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "xAxis": {"type": "value"},
+        "yAxis": {
+            "type": "category",
+            "data": years,
+        },
+        "series": series,
+    }
+    st_echarts(options=options, height="500px")
+
+
+def create_scatter_single_axis(df):
+    years = sorted(list(df["year"].astype(str).unique()))
+    months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
+
+    df["date"] = df["date"].str.replace(" ", "-")
+    # Convert the 'date' column to datetime format
+    df["date"] = pd.to_datetime(df["date"])
+
+    # Add a new column for the day of the week
+    df["day_of_week"] = df["date"].dt.day_name()
+
+    price_year_month = df.groupby(["year", "month"])["modal_rs_quintal"].agg("mean")
+    price_year_month_df = price_year_month.reset_index(name="avg_price")
+
+    price_data = []
+    year_idx = 0
+    for year in years:
+        month_idx = 0
+        for month in months:
+            price_month = []
+            price_month.append(year_idx)
+            price_month.append(month_idx)
+            avg_month_price = price_year_month_df.query(
+                f"year == {int(year)} and month == '{month}'"
+            )["avg_price"].values
+            if len(avg_month_price) == 0:
+                price_month.append(0)
+            else:
+                price = round(float(avg_month_price[0]))
+                price_month.append(price)
+            price_data.append(price_month)
+            month_idx += 1
+        year_idx += 1
+
+    # print("price_data", price_data)
+
+    option = {
+        "tooltip": {"position": "top"},
+        "title": [
+            {"textBaseline": "middle", "top": f"{(idx + 0.5) * 100 / 7}%", "text": year}
+            for idx, year in enumerate(years)
+        ],
+        "singleAxis": [
+            {
+                "left": 150,
+                "type": "category",
+                "boundaryGap": False,
+                "data": months,
+                "top": f"{(idx * 100 / 7 + 5)}%",
+                "height": f"{(100 / 7 - 10)}%",
+                "axisLabel": {"interval": 2},
+            }
+            for idx, _ in enumerate(years)
+        ],
+        "series": [
+            {
+                "singleAxisIndex": idx,
+                "coordinateSystem": "singleAxis",
+                "type": "scatter",
+                "data": [],
+                "symbolSize": JsCode(
+                    "function(data_item){return data_item[1]/60}"
+                ).js_code,
+            }
+            for idx, _ in enumerate(years)
+        ],
+    }
+    for data_item in price_data:
+        option["series"][data_item[0]]["data"].append([data_item[1], data_item[2]])
+
+    st_echarts(options=option, height="600px")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,26 @@
+import os
+
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-from src.create_statewise_dfs import get_statewise_datasets
+# from src.create_statewise_dfs import get_statewise_datasets
+import src.chart_renderers as chart
+
+DATA_PATH = "./data/5_yr_data"
+
+
+def read_states_data(path: str) -> dict[str, pd.DataFrame]:
+    print(os.listdir(path))
+    states_data: dict[str, pd.DataFrame] = dict()
+    for file in os.listdir(path):
+        states_data[file[:2].lower()] = pd.read_csv(path + "/" + file)
+    return states_data
 
 
 @st.cache_data
 def get_cached_states_data():
-    return get_statewise_datasets()
+    return read_states_data(DATA_PATH)
 
 
 states_data = get_cached_states_data()
@@ -16,17 +29,43 @@ states_data = get_cached_states_data()
 st.title("Analysis of Commodity Data")
 
 state = st.selectbox("Select State", list(states_data.keys()))
-commodity = st.selectbox("Select Commodity", ["Onion", "Other"])  # ... (your options)
-plot_type = st.selectbox("Plot Type", ["scatter", "bar"])
+commodity = st.selectbox(
+    "Select Commodity", states_data[state].commodity.unique()
+)  # ... (your options)
+plot_type = st.selectbox(
+    "Plot Type",
+    ["scatter", "bar", "line", "stacked line", "stacked bar", "scatter single axis"],
+)
 
 scatter_dims = []
 box_dims = []
+selector = list(states_data[state].columns[1:])
+year_wise_select = selector
+year_wise_select.remove("year")
+selector.remove("commodity")
+
+numeric_cols = []
+categorical_cols = []
+
+for column in states_data[state].columns:
+    if states_data[state][column].dtype == "O":
+        categorical_cols.append(column)
+    else:
+        numeric_cols.append(column)
+
+numeric_cols.remove("Unnamed: 0")
 if plot_type == "scatter":
-    scatter_dims = st.multiselect(
-        "Select Features for Scatter Plot", states_data[state].columns
-    )
+    scatter_dims = st.multiselect("Select Features for Scatter Plot", selector)
 elif plot_type == "bar":
-    box_dims = st.selectbox("Select Feature for Bar Chart", states_data[state].columns)
+    box_dims = st.selectbox("Select Feature for Bar Chart", selector)
+elif plot_type == "stacked line" or plot_type == "stacked bar":
+    stacked_by_dims = st.selectbox(
+        "Select Grouping Feature for  Line Chart", categorical_cols
+    )
+    stacked_col_dim = st.selectbox("Select Feature for Bar Chart", numeric_cols)
+elif plot_type == "line":
+    line_dim = st.selectbox("Select Feature for Bar Chart", numeric_cols)
+
 
 req_df = states_data[state]
 color = None
@@ -46,7 +85,7 @@ if plot_type == "scatter":
     if color:
         st.plotly_chart(
             px.scatter_matrix(
-                req_df[req_df.commodity == commodity][scatter_dims], color=req_df[color]
+                req_df[req_df.commodity == commodity][scatter_dims], color=color
             )
         )
     else:
@@ -62,8 +101,16 @@ if plot_type == "scatter":
             st.warning("Please select at least 2 features for the scatter plot.")
 elif plot_type == "bar":
     st.plotly_chart(
-        px.box(req_df[req_df.commodity == commodity], y=box_dims, color=color)
+        px.bar(req_df[req_df.commodity == commodity], y=box_dims, color=color)
     )
+elif plot_type == "line":
+    chart.create_line_chart(req_df, line_dim)
+elif plot_type == "stacked line":
+    chart.create_stacked_line_chart(req_df, stacked_by_dims, stacked_col_dim)
+elif plot_type == "stacked bar":
+    chart.create_stacked_bar_chart(req_df, stacked_by_dims, stacked_col_dim)
+elif plot_type == "scatter single axis":
+    chart.create_scatter_single_axis(req_df)
 else:
     st.write("Unsupported plot type!")
 
