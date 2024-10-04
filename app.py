3e95f26be43c7ae769ca273eccd0c2d074e344e8
mashriram <mashriram@gmail.com>
2024-10-04 21:23:33 +0530

    fix: app imports

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-import utils.chart_renderers as chart
+import src.utils.chart_renderers as chart
 
 DATA_PATH = "./data/5_yr_data"
 
